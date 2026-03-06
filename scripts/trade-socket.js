7fd67bd0d8d729e9bbdaca04733ff6ab3d50b9b5
NomadODST <konstantin.zahn@googlemail.com>
2026-03-06 18:44:15 +0100

    Update trade-socket.js

--- scripts/trade-socket.js
+++ scripts/trade-socket.js
@@ -1,13 +1,6 @@
 Hooks.once("ready", () => {
 
-  const ST = game.simpleTrade || globalThis.simpleTrade;
-
-  if (!ST) {
-    console.error("Simple Token Trade | Namespace missing");
-    return;
-  }
-
-  /* Socket Listener */
+  const ST = game.simpleTrade;
 
   game.socket.on("module.simple-token-trade", data => {
 
@@ -20,20 +13,25 @@ Hooks.once("ready", () => {
 
   });
 
-  /* Sync Function */
-
-  ST.sync = function (session) {
+  ST.sync = function(session) {
 
     game.socket.emit("module.simple-token-trade", {
+
       id: session.id,
+
       state: {
+
         offerA: session.offerA,
         offerB: session.offerB,
+
         goldA: session.goldA,
         goldB: session.goldB,
+
         acceptA: session.acceptA,
         acceptB: session.acceptB
+
       }
+
     });
 
   };
