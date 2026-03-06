dec95605223c39753621705795ff202b2168fcae
NomadODST <konstantin.zahn@googlemail.com>
2026-03-06 20:24:38 +0100

    Update trade-session.js

--- scripts/trade-session.js
+++ scripts/trade-session.js
@@ -1,41 +1,72 @@
-globalThis.simpleTrade = globalThis.simpleTrade || {};
+/**
+ * simple-token-trade | trade-session.js
+ * Manages the state of a single trade between two actors.
+ */
 
-class TradeSession {
+export class TradeSession {
+  /**
+   * @param {string} actorAId  - UUID or ID of the initiating actor
+   * @param {string} actorBId  - UUID or ID of the target actor
+   * @param {string} [id]      - Optional: reuse existing session ID (from socket)
+   */
+  constructor(actorAId, actorBId, id = null) {
+    this.id      = id ?? foundry.utils.randomID(16);
+    this.actorAId = actorAId;
+    this.actorBId = actorBId;
 
-  constructor(actorA, actorB) {
-
-    this.id = foundry.utils.randomID();
-
-    this.actorA = actorA;
-    this.actorB = actorB;
-
+    // Trade offers: array of { itemId, quantity }
     this.offerA = [];
     this.offerB = [];
 
+    // Gold amounts
     this.goldA = 0;
     this.goldB = 0;
 
+    // Accept states
     this.acceptA = false;
     this.acceptB = false;
 
+    // Register in global store
+    globalThis.simpleTrade.sessions[this.id] = this;
   }
 
-  open() {
+  /** Convenience getters */
+  get actorA() { return game.actors.get(this.actorAId); }
+  get actorB() { return game.actors.get(this.actorBId); }
 
-    this.app = new globalThis.simpleTrade.TradeApp(this);
-    this.app.render(true);
-
-    game.simpleTrade.sessions[this.id] = this;
-
-  }
-
+  /** Reset both accept states (called on any offer change) */
   resetAccept() {
-
     this.acceptA = false;
     this.acceptB = false;
-
   }
 
-}
+  /** Serialize for socket transmission */
+  toJSON() {
+    return {
+      id:       this.id,
+      actorAId: this.actorAId,
+      actorBId: this.actorBId,
+      offerA:   this.offerA,
+      offerB:   this.offerB,
+      goldA:    this.goldA,
+      goldB:    this.goldB,
+      acceptA:  this.acceptA,
+      acceptB:  this.acceptB,
+    };
+  }
 
-globalThis.simpleTrade.TradeSession = TradeSession;
+  /** Update local state from serialized socket data */
+  updateFromJSON(data) {
+    this.offerA  = data.offerA  ?? this.offerA;
+    this.offerB  = data.offerB  ?? this.offerB;
+    this.goldA   = data.goldA   ?? this.goldA;
+    this.goldB   = data.goldB   ?? this.goldB;
+    this.acceptA = data.acceptA ?? this.acceptA;
+    this.acceptB = data.acceptB ?? this.acceptB;
+  }
+
+  /** Destroy this session */
+  destroy() {
+    delete globalThis.simpleTrade.sessions[this.id];
+  }
+}
