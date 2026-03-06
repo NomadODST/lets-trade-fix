0412bf949b9f24f23a9c6c555cecbae613eddc9f
NomadODST <konstantin.zahn@googlemail.com>
2026-03-06 18:03:33 +0100

    Update trade-socket.js

--- scripts/trade-socket.js
+++ scripts/trade-socket.js
@@ -1,27 +1,30 @@
-Hooks.once("ready",()=>{
+Hooks.once("ready", () => {
 
-if(!game.simpleTrade.sessions)
-game.simpleTrade.sessions={};
+  game.socket.on("module.simple-token-trade", data => {
 
-game.socket.on("module.simple-token-trade",(data)=>{
+    const session = game.simpleTrade.sessions[data.id];
+    if (!session) return;
 
-const session=game.simpleTrade.sessions[data.id];
+    Object.assign(session, data.state);
 
-if(!session) return;
+    session.app.render();
 
-Object.assign(session,data.state);
+  });
 
-session.app.render();
+  game.simpleTrade.sync = function(session) {
 
-});
+    game.socket.emit("module.simple-token-trade", {
+      id: session.id,
+      state: {
+        offerA: session.offerA,
+        offerB: session.offerB,
+        goldA: session.goldA,
+        goldB: session.goldB,
+        acceptA: session.acceptA,
+        acceptB: session.acceptB
+      }
+    });
 
-game.simpleTrade.sync=function(session){
-
-game.socket.emit("module.simple-token-trade",{
-id:session.id,
-state:session.serialize()
-});
-
-};
+  };
 
 });
